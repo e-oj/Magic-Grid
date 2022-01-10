fb99ce9ede67fc02f3cfa02f5e5e9ed638f5237c
e-oj <eoo5498@rit.edu>
2022-01-11 00:44:11 +0100

    update tests

---
+++ test/modify-items.js
@@ -0,0 +1,38 @@
+/**
+ * @author emmanuelolaojo
+ * @since 2022-01-10
+ */
+
+function addItems(numItems=10) {
+  const container = document.querySelector(".container");
+  const items = container.children;
+  const lastItem = items[items.length - 1];
+  const lastNumber = parseInt(lastItem.innerHTML);
+
+  for (let i = 1; i <= numItems; i++){
+    const newDiv = document.createElement("div");
+    const newNumber = lastNumber + i;
+
+    newDiv.innerText = newNumber.toString();
+    container.appendChild(newDiv);
+    newDiv.classList.add(`item${newNumber}`);
+  }
+
+  magicGrid.positionItems();
+}
+
+
+function removeItems(numItems=10) {
+  const container = document.querySelector(".container");
+  const items = container.children;
+  const lastIndex = items.length - 1;
+  let stopIndex = lastIndex - numItems;
+
+  if (stopIndex < 0) stopIndex = 0;
+
+  for (let i = lastIndex; i > stopIndex; i--){
+    items[i].remove();
+  }
+
+  magicGrid.positionItems();
+}
