d07515d00d9f26fbf8faffa8d776abf5742cedfc
Harald Atteneder <harald@urbantrout.io>
2024-07-12 09:04:36 +0200

    remove items function

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ class MagicGrid extends EventEmitter{
     if (!this.ready()) return;
 
     this.container.style.position = "relative";
-    const items = this.items();
+    const items = this.items;
 
     for (let i = 0; i < items.length; i++) {
       if (this.styledItems.has(items[i])) continue;
@@ -72,23 +72,13 @@ class MagicGrid extends EventEmitter{
   }
 
   /**
-   * Gets a collection of all items in a grid.
-   *
-   * @return {HTMLCollection}
-   * @private
-   */
-  items () {
-    return this.container.children;
-  }
-
-  /**
    * Calculates the width of a column.
    *
    * @return width of a column in the grid
    * @private
    */
   colWidth () {
-    return this.items()[0].getBoundingClientRect().width + this.gutter;
+    return this.items[0].getBoundingClientRect().width + this.gutter;
   }
 
   /**
@@ -151,7 +141,7 @@ class MagicGrid extends EventEmitter{
     let { cols, wSpace } = this.setup();
     let maxHeight = 0;
     let colWidth = this.colWidth();
-    let items = this.items();
+    let items = this.items;
 
     wSpace = this.center ? Math.floor(wSpace / 2) : 0;
 
@@ -192,7 +182,7 @@ class MagicGrid extends EventEmitter{
    */
   ready () {
     if (this.static) return true;
-    return this.items().length >= this.size;
+    return this.items.length >= this.size;
   }
 
   /**
