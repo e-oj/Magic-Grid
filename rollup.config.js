65141ea3aa415eaeb88d67604f6bf88a3aa408de
Staria Olaojo <97202548+Esstar612@users.noreply.github.com>
2024-07-16 15:22:00 -0400

    Changed rollup config so code looks better

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import prettier from "rollup-plugin-prettier";
 import terser from "@rollup/plugin-terser";
 import pkg from "./package.json" assert { type: "json" };
 
@@ -9,7 +10,8 @@ export default [{
   output: {
     file: pkg.browser,
     format: "umd",
-    name: "MagicGrid"
+    name: "MagicGrid",
+    sourcemap: true
   },
   plugins: [
     resolve(),
@@ -18,18 +20,38 @@ export default [{
       exclude: "node_modules/**",
       babelHelpers: "bundled"
     }),
-    terser()
+    terser({
+      format: {
+        comments: true,
+      },
+      mangle: {
+        keep_classnames: true,
+        keep_fnames: true,
+      }
+    }) // Only terser for UMD minification
   ]
 }, {
   input: "src/index.js",
   output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" }
+    { file: pkg.main, format: "cjs", sourcemap: true },
+    { file: pkg.module, format: "es", sourcemap: true }
   ],
   plugins: [
     babel({
       exclude: "node_modules/**",
       babelHelpers: "bundled"
+    }),
+    prettier({
+      tabWidth: 2,
+      singleQuote: true,
+      bracketSpacing: true,
+      jsxBracketSameLine: false,
+      parser: "babel",
+      trailingComma: "none",
+      arrowParens: "always",
+      semi: true,
+      proseWrap: "always",
+      endOfLine: "lf"
     })
   ]
 }];
