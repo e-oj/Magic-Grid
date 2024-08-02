33a88cc45207787f12eae835b3f100486c1277e2
yuguangl <yuguanglee2003@gmail.com>
2024-08-02 13:07:29 -0400

    App.js modified to test better and more code from MagicGridComponent.js has been moved over to App.js such that MagicGridComponent.js now just returns the instance of MagicGrid

--- test/MagicGridComponent.js
+++ test/MagicGridComponent.js
@@ -1,34 +1,23 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import MagicGrid from 'magic-grid';
 import './MagicGrid.css';
 
 const useMagicGrid = (props) => {
     const magicGrid = useRef(null);
+    //const n = props.n;
     useEffect(() => {
         if (!magicGrid.current) {
-            console.log('useMagicGrid');
-            const grid = new MagicGrid({ container: `.${props.containerClass}`, ...props, items: props.items || 1, static: false });
+            console.log('useMagicGrid ', props.numItems);
+            const grid = new MagicGrid({container: `.container`, ...props, items: props.items || 1, static: false});
             magicGrid.current = grid;
             grid.listen();
-        }else{
-            console.log('positionItems');
+        } else if(magicGrid.current) {
+            console.log('positionItems ',props.numItems);
             magicGrid.current.positionItems();
         }
     }, [props]);
     return magicGrid.current;
 };
 
-const GridComponent = ({ containerClass, numItems }) => {
-    useMagicGrid({ containerClass, animate: true, gutter: 30, static: true, useMin: true, numItems });
-    return (
-        <div className={containerClass}>
-            {Array.from({length: numItems}, (_, index) => (
-                <div key={index} className={`grid-item item${index + 1}`}>
-                    Item {index + 1}
-                </div>
-            ))}
-        </div>
-    );
-};
+export default useMagicGrid;
 
-export default GridComponent;
--- test/ReactApp.js
+++ test/ReactApp.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import GridComponent from './MagicGridComponent';
+import React, {useState} from 'react';
+import useMagicGrid from './MagicGridComponent';
+
+const GridComponent = ({ containerClass, numItems }) => {
+    useMagicGrid({ containerClass, animate: true, gutter: 30, static: true, useMin: true, numItems });
+
+    //ngl im now exactly sure how this works
+    return (
+        <div className={containerClass}>
+            {Array.from({length: numItems}, (_, index) => (
+                <div key={index} className={`grid-item item${index + 1}`}>
+                    Item {index + 1}
+                </div>
+            ))}
+        </div>
+    );
+};
 
 const App = () => {
-    const [itemsp, setItems] = React.useState(10);
+    const [itemsp, setItems] = useState(10);
+    const [n, setN] = useState(0);
     return (
         <div className="App">
-            <button onClick={() => setItems(itemsp === 30 ? 10 : 30)}>Items</button>
+            <button onClick={() => setItems(itemsp === 5 ? 10 : 5)}>Items</button>
+            <button onClick={() => setN(n === 1 ? 0 : 1)}>Flip</button>
             <div className="grid-wrapper1">
-                <GridComponent containerClass="container" numItems={itemsp}/>
+                <GridComponent containerClass="container" numItems={itemsp} n = {n}/>
             </div>
             <div className="grid-wrapper1">
                 <GridComponent containerClass="container2" numItems={1}/>
