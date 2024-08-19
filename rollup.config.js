f6b8043c8bc211c352b4428ce03b26cef1e33b2b
e-oj <ooolaojo@gmail.com>
2024-08-19 16:38:48 -0700

    don't rely on browser option in package.json

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,11 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const babel = require("@rollup/plugin-babel");
 const terser = require("@rollup/plugin-terser");
-const pkg = require("./package.json");
 
 module.exports = [{
   input: "src/index.js",
   output: {
-    file: pkg.browser,
+    file: "dist/magic-grid.min.js",
     format: "umd",
     name: "MagicGrid"
   },
